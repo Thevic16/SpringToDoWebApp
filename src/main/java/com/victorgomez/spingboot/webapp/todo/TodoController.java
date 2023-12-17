830ec99226785172ce65b4baa5f904199c1da1be
Victor <vgomez@avathartech.com>
2023-12-17 17:55:35 -0400

    Step 25 - Implementing Update Todo - 1 - Show Update Todo Page

--- src/main/java/com/victorgomez/spingboot/webapp/todo/TodoController.java
+++ src/main/java/com/victorgomez/spingboot/webapp/todo/TodoController.java
@@ -1,7 +1,9 @@
 package com.victorgomez.spingboot.webapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,17 +32,41 @@ public class TodoController {
     }
 
     @RequestMapping(value="add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage(){
+    public String showNewTodoPage(ModelMap model){
+        String username = (String) model.get("name");
+        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.put("todo", todo);
+
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo(@RequestParam String description, ModelMap model){
-        String username = (String) model.get("name");
-        todoService.addTodo(username, description,
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result){
+        if(result.hasErrors()) {
+            return "todo";
+        }
+
+        String username = (String)model.get("name");
+        todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
+
         return "redirect:list-todos";
     }
 
+    @RequestMapping("delete-todo")
+    public String deleteTodo(@RequestParam int id) {
+        //Delete todo
+
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+
+    }
+
+    @RequestMapping("update-todo")
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
 
 }
